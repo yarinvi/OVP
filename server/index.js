beb5bfbfe9dc5fa5e76e1baa5580a9754ab6b227
yarinvi <yarin559900@gmail.com>
2025-03-11 11:46:32 +0200

    einav

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const routes = require("./routes/index");
 const connectDB = require("./lib/connect");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", 'https://ovp-h8sz.onrender.com'], credentials: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use("/api", routes);
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname,'public','index.html'));
+});
 
 app.listen(3000, () => {
   connectDB();
